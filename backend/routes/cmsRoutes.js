04db291047025556c9f77585c4125bc934676890
Chandan Kumar <chandan36024@gmail.com>
2026-03-30 23:59:21 +0530

    cmRoutes updating

--- backend/routes/cmsRoutes.js
+++ backend/routes/cmsRoutes.js
@@ -166,7 +166,7 @@ router.post('/subscribe', async (req, res, next) => {
         await sendWelcomeEmail(email);
         return res.status(200).json({ success: true, message: 'Welcome back! You have re-subscribed.' });
       }
-      return res.status(200).json({ success: true, message: 'Already subscribed' });
+      return res.status(400).json({ success: false, message: 'You are already subscribed to our newsletter.' });
     }
 
     const newSubscriber = new Subscriber({ email });
